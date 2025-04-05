593a0cb222a3f234d0f723d03c89570c2ed22326
christianlarry <christianlarry03@gmail.com>
2025-04-05 21:59:29 +0800

    update on main

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,8 @@ dotenv.config()
 import { web } from "@application/web"
 import { logger } from "@application/logging"
 
-import {client, connectToMongoDB} from "@application/database"
-
 // ENV IMPORT
 const PORT = process.env.PORT
-
-connectToMongoDB()
-
-web.get("/",async (req,res)=>{
-
-  await logger.info( await client.db().collections())
-
-  res.json({
-    "message": "hello world!"
-  }).end()
-})
 
 web.listen(PORT, ()=>{
   logger.info(`Server up and running at http://localhost:${PORT}`)
