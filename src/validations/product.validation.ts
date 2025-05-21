1a126cd3e0ece3f5a72adbed7137369ebffd3920
christianlarry <christianlarry03@gmail.com>
2025-05-22 01:33:35 +0800

    Transform value to capitalize

--- src/validations/product.validation.ts
+++ src/validations/product.validation.ts
@@ -1,4 +1,5 @@
 import { validationsStrings } from "@/constants/validations.strings"
+import { capitalize } from "@/utils/stringFormatter"
 import {z} from "zod"
 
 export const postProductValidation = z.object({
@@ -6,18 +7,18 @@ export const postProductValidation = z.object({
   description: z.string().optional(),
   type: z.enum(["Lantai", "Dinding"], {
     message: validationsStrings.product.typeRequired
-  }),
-  design: z.string().min(1, validationsStrings.product.designRequired),
+  }).transform(val=>capitalize(val)),
+  design: z.string().min(1, validationsStrings.product.designRequired).transform(val=>capitalize(val)),
   size_width: z.number().positive(validationsStrings.product.sizeWidthPositive),
   size_height: z.number().positive(validationsStrings.product.sizeHeightPositive),
-  color: z.string().min(1, validationsStrings.product.colorRequired),
-  finishing: z.string().min(1, validationsStrings.product.finishingRequired),
-  texture: z.string().min(1, validationsStrings.product.textureRequired),
-  brand: z.string().min(1, validationsStrings.product.brandRequired),
+  color: z.string().min(1, validationsStrings.product.colorRequired).transform(val=>capitalize(val)),
+  finishing: z.string().min(1, validationsStrings.product.finishingRequired).transform(val=>capitalize(val)),
+  texture: z.string().min(1, validationsStrings.product.textureRequired).transform(val=>capitalize(val)),
+  brand: z.string().min(1, validationsStrings.product.brandRequired).transform(val=>capitalize(val)),
   price: z.number().nonnegative(validationsStrings.product.priceNonNegative),
   is_slip_resistant: z.boolean(),
   is_water_resistant: z.boolean(),
-  recommended: z.array(z.string()).optional()
+  recommended: z.array(z.string().transform(val=>capitalize(val))).optional()
 })
 
 export const putProductValidation = postProductValidation
